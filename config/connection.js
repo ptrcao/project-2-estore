bcd229ded0048c0d682be7ed12fbd32c42a32a81
ptrcao <ptrcao@gmail.com>
2023-03-04 19:51:55 +1100

    Correct path ./.env --> ../.env in config\connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,8 @@
-require('dotenv').config({ path: './.env' });
+
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+// require('dotenv').config({ path: '../.env' });
+
 const Sequelize = require('sequelize');
 
 // Create a connection object
