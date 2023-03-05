3063c9a6efdeff0fcd775f73f58daf795b533f78
Jason Yoo <jason@jasons-mbp.home>
2023-03-06 01:15:52 +1100

    payment.js has been modified

--- controllers/api/payment.js
+++ controllers/api/payment.js
@@ -1,16 +1,29 @@
 const router = require('express').Router();
-const { Product } = require('../../models');
+const { Cart, Product } = require('../../models');
 
-router.get('/checkout', async (req, res) => {
+router.get('/payment', async (req, res) => {
   try {
-    // Fetch all products from the database
-    const products = await Product.findAll();
+    const userId = req.session.userId; // Get the ID of the current user from the session
 
-    // TODO: Implement logic to validate the user's cart and create an order object
-    const order = { /* order data */ };
+    // Fetch the user's cart from the database
+    const cart = await Cart.findOne({
+      where: { userId },
+      include: [{ model: Product }],
+    });
+
+    // Ensure that all products in the cart are still available and have not been modified
+    for (const item of cart.products) {
+      const product = item.product;
+      if (product.stock < item.quantity) {
+        return res.status(400).json({ message: `Product ${product.id} is out of stock` });
+      }
+    }
+
+    // TODO: Create an order object and save it to the database
 
     // Redirect the user to the payment page with the order ID
     res.redirect(`https://payment-processor.com/pay?orderId=${order.id}`);
+    // URL should be updated accordingly
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server error' });
